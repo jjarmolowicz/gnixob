a3f6b59bdf53fa125c68425a5f8091e0d441fbd5
Dominik Plichta <dominik.plichta@tomtom.com>
2019-01-08 20:30:18 +0100

    initial gdx setup

---
+++ desktop/src/fun/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fun.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fun.game.Gnixob;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gnixob(), config);
+	}
+}
